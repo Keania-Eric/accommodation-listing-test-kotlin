ad524e79ac339022f4b424dec49971394f5dc6be
Keania Eric <adiatagbalaya@ADIATs-MBP.lan>
2023-08-07 10:59:44 +0100

    project initialization - initial commit

---
+++ src/main/kotlin/com/trivago/accomodationlisting/AccomodationlistingApplication.kt
@@ -0,0 +1,11 @@
+package com.trivago.accomodationlisting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccomodationlistingApplication
+
+fun main(args: Array<String>) {
+    runApplication<AccomodationlistingApplication>(*args)
+}
---
+++ src/test/kotlin/com/trivago/accomodationlisting/AccomodationlistingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trivago.accomodationlisting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccomodationlistingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
